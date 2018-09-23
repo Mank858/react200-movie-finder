191ea89c69b2a32b31a3e9cd41f35967adceee40
jake espino <daniel858espino@gmail.com>
2018-09-22 18:41:15 -0700

    fixes actions

--- src/js/Components/Search/searchActions.js
+++ src/js/Components/Search/searchActions.js
@@ -12,9 +12,9 @@ export function updateSearch(searchInput) {
 export function getMovie(searchInput) {
   return {
     type: 'GET_MOVIE',
-    payload: axios.get(`http://www.omdbapi.com/?s=${searchInput}&apikey=8730e0e`)
+    payload: axios.get(`https://www.omdbapi.com/?s=${searchInput}&apikey=8730e0e`)
       .then((response) => {
-        const results = response.data.Search.map(Arr => axios.get(`http://www.omdbapi.com/?i=${Arr.imdbID}&apikey=8730e0e`));
+        const results = response.data.Search.map(Arr => axios.get(`https://www.omdbapi.com/?i=${Arr.imdbID}&apikey=8730e0e`));
         return axios.all(results)
         .then(res2 => res2);
       })
